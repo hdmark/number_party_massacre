c01be6ef953aab43a84a0f0a17cf5a91725329bd
mark baskin <mark.d.baskin@gmail.com>
2022-09-14 20:24:40 -0400

    initial table working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import MainTable from "./components/table"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Number Party Massacre
       </header>
+      <div className="App-content">
+        Body content
+        <MainTable />
+      </div>
+      <footer className="App-footer"> Thanks for visiting</footer>
     </div>
   );
 }
